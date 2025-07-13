4fa2100b4a55581d1112e792e3e841308dd79565
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-07-13 23:55:27 +0700

    Update: Decr timeout jwt

--- app/core/config.py
+++ app/core/config.py
@@ -18,8 +18,8 @@ class Settings(BaseSettings):
     APP_VERSION: str = "1.0.0"
     PAGE_SIZE: int = 10
     # Secret
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 1
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 5
     ACCESS_KEY: str
     REFRESH_KEY: str
     # FrontEnd
