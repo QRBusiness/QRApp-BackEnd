b0c0c3a156ab008fc29bc19656f24c1fddb67389
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-07-14 20:03:19 +0700

    feat(branch): oncasade user

--- app/api/router/branch/__init__.py
+++ app/api/router/branch/__init__.py
@@ -6,12 +6,12 @@ from fastapi import APIRouter, Depends, Request
 from app.api.dependency import (login_required, required_permissions,
                                 required_role)
 from app.common.api_response import Response
-from app.common.http_exception import (HTTP_400_BAD_REQUEST,
-                                       HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND,
+from app.common.http_exception import (HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND,
                                        HTTP_409_CONFLICT)
+from app.db import Mongo
 from app.schema.branch import (BranchCreateWithoutBusiness, BranchResponse,
                                BranchUpdate)
-from app.service import branchService, businessService
+from app.service import branchService, businessService, userService
 
 apiRouter = APIRouter(
     tags=["Branch"],
@@ -96,13 +96,16 @@ async def update_branch(id: PydanticObjectId, data: BranchUpdate, request: Reque
     ]
 )
 async def delete_branch(id: PydanticObjectId, request: Request):
-    branch = await branchService.find(id)
-    if branch is None:
-        raise HTTP_404_NOT_FOUND("Không tìm thấy chi nhánh")
-    branch_scope = branch.business.to_ref().id
-    user_scope = PydanticObjectId(request.state.user_scope)
-    if branch_scope != user_scope:
-        raise HTTP_403_FORBIDDEN("Bạn không đủ quyền thực hiện hành động này")
-    if not await branchService.delete(id):
-        raise HTTP_400_BAD_REQUEST("Lỗi")
-    return Response(data="Xóa thành công")
+    async with businessService.transaction(Mongo.client) as session:
+        branch = await branchService.find(id,session=session)
+        if branch is None:
+            raise HTTP_404_NOT_FOUND("Không tìm thấy chi nhánh")
+        branch_scope = branch.business.to_ref().id
+        user_scope = PydanticObjectId(request.state.user_scope)
+        if branch_scope != user_scope:
+            raise HTTP_403_FORBIDDEN("Bạn không đủ quyền thực hiện hành động này")
+        await branchService.delete(id,session=session)
+        await userService.delete_many(conditions={
+            "branch.$id":id
+        })
+        return Response(data="Xóa thành công")
