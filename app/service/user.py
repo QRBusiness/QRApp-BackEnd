3afda4d8d0ca136d9e415b121e27fbbe7bc63603
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-07-28 10:49:45 +0700

    Fix(user): Refactor permission conditions for Staff role

--- app/service/user.py
+++ app/service/user.py
@@ -38,13 +38,11 @@ class UserService(Service[User, UserCreate, UserUpdate]):
         if data["role"] == "Staff":
             permissions = await permissionService.find_many(
                 conditions={
-                    "code": {
-                        "$in": [
-                            {"$regex": r"^view.*(area|branch|order|category|subcategory)$"},
-                            {"$regex": r"^view.*(serviceunit|product|request)$"},
-                            {"$regex": r"^update.*(order|request)$"},
-                        ]
-                    }
+                    "$or": [
+                        {"code": {"$regex": r"^view.*(area|branch|order|category|subcategory)$"}},
+                        {"code": {"$regex": r"^view.*(serviceunit|product|request)$"}},
+                        {"code": {"$regex": r"^update.*(order|request)$"}},
+                    ]
                 },
                 session=session,
             )
