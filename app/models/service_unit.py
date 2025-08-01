ce0ecc644d504a80973f7d3b3c7507fe808e7e6e
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-08-01 17:00:39 +0700

    QRCode: None

--- app/models/service_unit.py
+++ app/models/service_unit.py
@@ -10,9 +10,7 @@ from app.models.base import Base
 
 class ServiceUnit(Base):
     name: str = Field(...)
-    qr_code: Optional[str] = Field(
-        default="https://t3.ftcdn.net/jpg/05/79/68/24/360_F_579682465_CBq4AWAFmFT1otwioF5X327rCjkVICyH.jpg",
-    )
+    qr_code: Optional[str] = Field(default=None)
     available: bool = Field(default=True)
     area: Link[Area] = Field(...)
     branch: Link[Branch] = Field(...)
