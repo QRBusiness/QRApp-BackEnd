7fd0c5fdd399264ed91071bcf97be944e3746a5d
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-08-02 14:17:20 +0700

    serialize_qr_code

--- app/schema/service_unit.py
+++ app/schema/service_unit.py
@@ -1,8 +1,10 @@
 from typing import Optional
+from urllib.parse import urlparse, urlunparse
 
 from beanie import PydanticObjectId
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer
 
+from app.core.config import settings
 from app.schema import BaseResponse
 from app.schema.area import AreaResponse
 
@@ -20,3 +22,13 @@ class ServiceUnitResponse(BaseResponse):
     name: str
     qr_code: Optional[str] = None
     area: Optional[AreaResponse] = None
+
+    @field_serializer("img_url")
+    def serialize_qr_code(self, value: Optional[str]) -> Optional[str]:
+        if value is not None:
+            parsed = urlparse(value)
+            if parsed.netloc == settings.MINIO_ENDPOINT:
+                base = urlparse(settings.BASE_URL)
+                new_url = parsed._replace(netloc=base.netloc, scheme=base.scheme)
+                return urlunparse(new_url)
+        return value
