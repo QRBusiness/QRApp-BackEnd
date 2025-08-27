cb22dd821a388da743ac44c1d940c9eb5792b764
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-08-27 23:07:27 +0700

    Use domain

--- app/main.py
+++ app/main.py
@@ -71,7 +71,13 @@ app.add_middleware(TraceMiddleware)
 app.add_middleware(LoggingMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        settings.FRONTEND_HOST,
+        # Add production domains
+        # "https://yourdomain.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
