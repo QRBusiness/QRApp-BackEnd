aa5a997fcf821ad671980de68cf9f7bf073f3d94
Van Ngoc Nhat Huy <nhathuyd4hp@gmail.com>
2025-09-28 19:49:45 +0700

    CRUD VOucher

--- app/api/router/voucher/__init__.py
+++ app/api/router/voucher/__init__.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends, Request
+from beanie import PydanticObjectId
+from fastapi import APIRouter, Depends, Query, Request
 
-from app.api.dependency import login_required
+from app.api.dependency import login_required, permission_required
 from app.common.api_response import Response
+from app.core.config import settings
 from app.schema.voucher import VoucherCreate
 from app.service import voucherService
 
@@ -14,12 +16,56 @@ apiRouter = APIRouter(
 )
 
 
+@apiRouter.get(
+    path="/",
+    name="Danh sách khuyến mãi",
+    response_model=Response,
+    dependencies=[
+        Depends(permission_required(permissions=["view.voucher"])),
+    ],
+)
+async def get_vouchers(
+    request: Request,
+    page: int = Query(default=1, ge=1),
+    limit: int = Query(default=settings.PAGE_SIZE, ge=1, le=200),
+):
+    conditions = {"business.$id": PydanticObjectId(request.state.user_scope)}
+    vouchers = await voucherService.find_many(
+        conditions=conditions,
+        skip=(page - 1) * limit,
+        limit=limit,
+    )
+    return Response(data=vouchers)
+
+
+@apiRouter.get(
+    path="/{id}",
+    name="Danh sách khuyến mãi",
+    response_model=Response,
+    dependencies=[
+        Depends(permission_required(permissions=["view.voucher"])),
+    ],
+)
+async def get_voucher(
+    request: Request,
+    id: PydanticObjectId,
+):
+    conditions = {
+        "id": id,
+        "business.$id": request.state.user_scope,
+    }
+    voucher = await voucherService.find_one(
+        conditions=conditions,
+    )
+    return Response(data=voucher)
+
+
 @apiRouter.post(
     path="/",
     name="Tạo khuyến mãi",
     response_model=Response,
     dependencies=[
-        Depends(login_required),
+        Depends(permission_required(permissions=["create.voucher"])),
     ],
 )
 async def post_voucher(data: VoucherCreate, request: Request):
